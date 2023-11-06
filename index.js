db963450c987975c0bb72e75088a64fe90bb8dad
muskbuster <sudeepkamat79@gmail.com>
2023-11-06 10:18:54 +0530

    index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const db = require('./schema/db.js'); // Path to your db.js file
 
 // Import your routes
-const router = require('G:/Backend_sui/routes/routes.js');
+const router = require('./routes/routes.js');
 
 // Middleware for parsing JSON requests
 app.use(express.json());
@@ -17,5 +17,5 @@ app.use('/bets', router);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is listening on port 3000`);
+  console.log(`Server is listening on port 4000`);
 });
